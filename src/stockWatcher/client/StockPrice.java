2b64739f67e201d7cbe269bf9920d187306c1f4c
jarekjaro <krzeminskijarek@gmail.com>
2016-03-06 14:07:31 +0100

    added random prices genarator

---
+++ src/stockWatcher/client/StockPrice.java
@@ -0,0 +1,47 @@
+package stockWatcher.client;
+
+/**
+ * Created by Jaro on 2016-03-06.
+ */
+public class StockPrice {
+    private String symbol;
+    private double price;
+    private double change;
+
+    public StockPrice() {
+    }
+
+    public StockPrice(String symbol, double price, double change) {
+        this.symbol = symbol;
+        this.price = price;
+        this.change = change;
+    }
+
+    public String getSymbol() {
+        return this.symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getChange() {
+        return this.change;
+    }
+
+    public void setChange(double change) {
+        this.change = change;
+    }
+
+    public double getChangePercent() {
+        return 10.0 * this.change / this.price;
+    }
+}
